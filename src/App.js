9f60b1634c34a425cb48ae68f714306f2cf902e2
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-02-24 20:41:07 +0100

    feat: criate banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { NavBar } from './Components/NavBar';
+import { Banner } from './Components/Banner';
+import { Skills } from './Components/Skills';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
+      <Skills />
     </div>
   );
 }
